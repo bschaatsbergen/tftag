74f4048cb97d99c06b06a92a7d6312f2321829a6
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-03-26 13:42:37 +0200

    Set Apple

--- pkg/core/core_test.go
+++ pkg/core/core_test.go
@@ -125,7 +125,7 @@ resource "aws_s3_bucket" "users" {
   bucket = "users-bucket"
   tags = {
     BusinessUnit = "Finance"
-    Pine         = "Tree"
+    Pine         = "Apple"
   }
 }
 
@@ -133,7 +133,7 @@ resource "google_workstations_workstation_cluster" "test" {
   workstation_cluster_id = "workstation-cluster"
   labels = {
     BusinessUnit = "Finance"
-    Pine         = "Tree"
+    Pine         = "Apple"
   }
 }
 `,
